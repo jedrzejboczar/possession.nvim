7b1e8125d46eeac31709d38bea6cc25c32856db1
Jędrzej Boczar <yendreij@gmail.com>
2022-03-26 16:10:04 +0100

    chore: remove config options that are not yet implemented

--- lua/possession/config.lua
+++ lua/possession/config.lua
@@ -16,15 +16,11 @@ local function defaults()
             delete = 'PossessionDelete',
             list = 'PossessionList',
         },
-        close_floating_windows = true,
         hooks = {
             before_save = function(name) return {} end,
             after_save = function(name, user_data, aborted) end,
             before_load = function(name, user_data) return user_data end,
             after_load = function(name, user_data) end,
-        },
-        plugins = {
-            nvim_tree = false,
         },
     }
 end
