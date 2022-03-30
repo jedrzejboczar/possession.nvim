1a5df7829be730f752075919af4918de0aeb4ff4
Jędrzej Boczar <yendreij@gmail.com>
2022-03-30 22:26:50 +0200

    fix(plugins): make sure to always pass config table to a plugin

--- lua/possession/plugins/init.lua
+++ lua/possession/plugins/init.lua
@@ -10,13 +10,22 @@ local function req(plugin)
     return require('possession.plugins.' .. plugin)
 end
 
-local function hook_active(plugin, hook)
+local function get_config(plugin)
     local c = config.plugins[plugin]
+    vim.validate { config = { c, { 'boolean', 'table' } } }
     if type(c) == 'boolean' then
-        return c
-    else
-        return c and c.hooks == nil or vim.tbl_contains(c.hooks, hook)
+        return c and {} or nil
     end
+    return c
+end
+
+local function hook_active(plugin, hook)
+    local c = get_config(plugin)
+    if not c then
+        return false
+    end
+    -- If `hooks` is missing then all hooks are enabled
+    return c.hooks == nil or vim.tbl_contains(c.hooks, hook)
 end
 
 local function get_enabled(hook)
@@ -28,7 +37,7 @@ end
 function M.before_save(name)
     local plugin_data = {}
     for _, p in ipairs(get_enabled('before_save')) do
-        local data = req(p).before_save(config.plugins[p], name)
+        local data = req(p).before_save(get_config(p), name)
         -- Plugin can abort saving
         if not data then
             return nil
@@ -42,13 +51,13 @@ end
 
 function M.after_save(name, plugin_data, aborted)
     for _, p in ipairs(get_enabled('after_save')) do
-        req(p).after_save(config.plugins[p], name, plugin_data[p], aborted)
+        req(p).after_save(get_config(p), name, plugin_data[p], aborted)
     end
 end
 
 function M.before_load(name, plugin_data)
     for _, p in ipairs(get_enabled('before_load')) do
-        local data = req(p).before_load(config.plugins[p], name, plugin_data[p])
+        local data = req(p).before_load(get_config(p), name, plugin_data[p])
         if not data then
             return nil
         else
@@ -60,7 +69,7 @@ end
 
 function M.after_load(name, plugin_data)
     for _, p in ipairs(get_enabled('after_load')) do
-        req(p).after_load(config.plugins[p], name, plugin_data[p])
+        req(p).after_load(get_config(p), name, plugin_data[p])
     end
 end
 
