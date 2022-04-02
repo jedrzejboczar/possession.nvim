718513c2b4b6b242622bb3e77c1ba5c1f8b47fd6
Jędrzej Boczar <yendreij@gmail.com>
2022-04-02 15:59:56 +0200

    style: format with stylua

--- lua/possession/plugins/tabby.lua
+++ lua/possession/plugins/tabby.lua
@@ -5,12 +5,14 @@ local utils = require('possession.utils')
 local has_tabby = pcall(require, 'tabby')
 
 function M.before_save(opts, name)
-    if not has_tabby then return {} end
+    if not has_tabby then
+        return {}
+    end
 
     local tab_names = {}
 
     for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
-        local fallback_nil =  function()
+        local fallback_nil = function()
             return nil
         end
         -- We must use string keys or else json.encode may assume it's a list
@@ -19,12 +21,14 @@ function M.before_save(opts, name)
     end
 
     return {
-        tab_names = tab_names
+        tab_names = tab_names,
     }
 end
 
 function M.after_load(opts, name, plugin_data)
-    if not has_tabby then return end
+    if not has_tabby then
+        return
+    end
 
     local num2id = utils.tab_num_to_id_map()
     for num, tab_name in pairs(plugin_data.tab_names or {}) do
