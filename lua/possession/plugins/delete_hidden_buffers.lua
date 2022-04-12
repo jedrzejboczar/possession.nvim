ac58abd802625a67005e4c515f2d0383bbf8d904
Jędrzej Boczar <yendreij@gmail.com>
2022-04-12 22:30:24 +0200

    fix: inverted logic for hidden buffers in delete_hidden_buffers

--- lua/possession/plugins/delete_hidden_buffers.lua
+++ lua/possession/plugins/delete_hidden_buffers.lua
@@ -7,7 +7,9 @@ local utils = require('possession.utils')
 -- `opts` should have the same format as `config.plugins.delete_hidden_buffers`.
 function M.delete_hidden_buffers(opts)
     local visible = utils.list_to_set(vim.tbl_map(vim.api.nvim_win_get_buf, vim.api.nvim_list_wins()))
-    local hidden = vim.tbl_filter(utils.getter(visible), vim.api.nvim_list_bufs())
+    local hidden = vim.tbl_filter(function(buf)
+        return not visible[buf]
+    end, vim.api.nvim_list_bufs())
 
     for _, buf in ipairs(hidden) do
         if not pcall(vim.api.nvim_buf_delete, buf, { force = opts.force }) then
